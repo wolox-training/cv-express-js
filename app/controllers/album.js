20b58003d23f46eda6e48ca5785146689cb861c3
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2020-05-12 13:35:12 -0500

    delete extra lines on functions

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -1,12 +1,12 @@
 const { albumsByUser, photosByAlbum } = require('../services/album');
 
-module.exports.getAlbums = (_, res, next) => {
+exports.getAlbums = (_, res, next) => {
   albumsByUser()
     .then(data => res.status(200).send({ books: data }))
     .catch(next);
 };
 
-module.exports.getPhotos = (req, res, next) => {
+exports.getPhotos = (req, res, next) => {
   photosByAlbum(req.params)
     .then(data => res.status(200).send({ photos: data }))
     .catch(next);
