0447a3923a495e1640a7418b78189667d14d3b8f
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2020-05-13 15:45:43 -0500

    change model names to camelCase

--- app/models/user.js
+++ app/models/user.js
@@ -2,11 +2,11 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'users',
     {
-      first_name: {
+      firstName: {
         type: DataTypes.STRING,
         allowNull: false
       },
-      last_name: {
+      lastName: {
         type: DataTypes.STRING,
         allowNull: false
       },
