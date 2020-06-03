4c2b32f024021c5f398680f14504600acf6a620b
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2020-06-03 10:53:03 -0500

    add schema for photos

--- documentation/schemas/album.js
+++ documentation/schemas/album.js
@@ -26,5 +26,41 @@ module.exports = {
         }
       }
     }
+  },
+  Photo: {
+    type: 'object',
+    properties: {
+      albumId: {
+        type: 'integer',
+        example: 2
+      },
+      id: {
+        type: 'integer',
+        example: 97
+      },
+      title: {
+        type: 'string',
+        example: 'labore magnam officiis nemo et'
+      },
+      url: {
+        type: 'string',
+        example: 'https://via.placeholder.com/600/e2223e'
+      },
+      thumbnailUrl: {
+        type: 'string',
+        example: 'https://via.placeholder.com/150/e2223e'
+      }
+    }
+  },
+  Photos: {
+    type: 'object',
+    properties: {
+      photos: {
+        type: 'array',
+        items: {
+          $ref: '#/components/schemas/Photo'
+        }
+      }
+    }
   }
 };
